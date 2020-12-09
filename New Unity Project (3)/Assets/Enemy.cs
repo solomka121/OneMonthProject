3284d6457e89eae5db2d01ed954af2af70a0c8d9
solomka121 <slavasolomahin12@gmail.com>
2020-12-09 18:33:42 +0200

    Added enemy+TestSpawner

---
+++ New Unity Project (3)/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int MaxHealth;
+    [SerializeField] private int CurrentHealth;
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    public void GetDamage(int damage)
+    {
+
+    }
+}
---
+++ New Unity Project (3)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody RB;
+
+    private float SpawnTime = 0.5f;
+    private float TimeToSpawn;
+
+    public GameObject gg;
+    public GameObject spawnEffect;
+
+    [SerializeField] private float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Invoke("mg" , 3);
+         
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horszontal");
+
+        Vector3 movementVec = new Vector3(horizontal , 0 , vertical);
+
+        //gameObject.transform.Translate(movementVec * speed * Time.deltaTime);
+        //RB.velocity = movementVec * speed;
+        RB.AddForce(movementVec * speed * 100);
+
+        if (TimeToSpawn <= 0)
+        {
+            GameObject box = Instantiate(gg , transform.position + new Vector3(Random.Range(1 , 8) , 20, Random.Range(1, 8)) , Quaternion.identity);
+            Instantiate(spawnEffect, box.transform.position, Quaternion.Euler(new Vector3(-90 , 0 , 0)));
+            TimeToSpawn = SpawnTime;
+
+            Instantiate(box , transform.position , Quaternion.identity);
+        }
+        else
+        {
+            TimeToSpawn -= Time.deltaTime;
+        }
+    }
+    public void mg()
+    {
+        print("hh");
+    }
+}
