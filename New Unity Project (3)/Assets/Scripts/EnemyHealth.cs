2caa586049bf47ee5116c30804742dee2613ed3e
solomka121 <slavasolomahin12@gmail.com>
2020-12-21 16:30:41 +0200

    added few effects (TO DO)

--- New Unity Project (3)/Assets/Scripts/EnemyHealth.cs
+++ New Unity Project (3)/Assets/Scripts/EnemyHealth.cs
@@ -9,18 +9,22 @@ public class EnemyHealth : MonoBehaviour
 
     [SerializeField] private GameObject _damageEffect;
 
+    Vector3 tt = Vector3.zero;
+
     private void Start()
     {
         _currentHealth = _maxHealth;
     }
     private void Update()
     {
-       
+        Debug.DrawLine(Vector3.zero , tt);
     }
 
     public void GetDamage(int damage , Vector3 hitPoint)
     {
         _currentHealth -= damage;
+        tt = hitPoint;
+        Instantiate(_damageEffect, transform.position, Quaternion.identity);
         if (_currentHealth <= 0) Death();
     }
     private void Death()
